86e28c74b1ed49b177b39a063bdd5a963be1739c
TeamKiwi <llafteamkiwi@gmail.com>
2020-11-23 14:36:36 +0100

    Start

---
+++ menunaviguation.php
@@ -0,0 +1,23 @@
+<!-- Menu de Naviguation -->
+	<nav class="menu-nav">
+		<ul>
+			<li class="bouton">
+				<a href="#">
+					Accueil
+				</a>
+			</li>
+
+			<li class="bouton">
+				<a href="">
+					Nous rejoindre
+				</a>
+			</li>
+
+			<li class="bouton">
+				<a href="Nous contacter.html">
+					Nous contacter
+				</a>
+			</li>
+
+		</ul>
+	</nav>
